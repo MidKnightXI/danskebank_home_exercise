d8a59816acc80a3236f90114ccb2c54473aeaca6
MidKnightXI <romain.bouguet@epitech.eu>
2025-06-14 18:24:42 +0200

    feat: removing relation with User object from the entity

--- DanskeBank.Communication/Databases/Entities/UserEntity.cs
+++ DanskeBank.Communication/Databases/Entities/UserEntity.cs
@@ -1,8 +1,8 @@
-using DanskeBank.Communication.Models;
-
 namespace DanskeBank.Communication.Databases.Entities;
 
-public class UserEntity : User
+public class UserEntity
 {
     public required Guid Id { get; set; }
+    public required string Email { get; set; }
+    public required string Password { get; set; }
 }
