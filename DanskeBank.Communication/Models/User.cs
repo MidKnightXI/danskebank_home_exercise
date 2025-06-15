02ee9eb3fb7a1aabc1df470a97664ada905d9aca
MidKnightXI <romain.bouguet@epitech.eu>
2025-06-15 17:11:56 +0200

    fix: removing JsonIgnore from the password

--- DanskeBank.Communication/Models/User.cs
+++ DanskeBank.Communication/Models/User.cs
@@ -1,10 +1,7 @@
-using System.Text.Json.Serialization;
-
 namespace DanskeBank.Communication.Models;
 
 public class User
 {
     public required string Email { get; set; }
-    [JsonIgnore]
     public required string Password { get; set; }
 }
